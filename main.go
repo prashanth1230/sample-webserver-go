fd1edac623edc21fd547d52b78ec856207f580db
Udupa, Prashanth <prashanth1230@gmail.com>
2021-09-06 17:42:43 +0530

    Added a query parameter names "name"

--- main.go
+++ main.go
@@ -7,7 +7,12 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		rw.Write([]byte("Hello World"))
+		names := r.URL.Query()["name"]
+		var name string
+		if len(names) != 0 {
+			name = names[0]
+		}
+		rw.Write([]byte("Hello " + name))
 	})
 
 	err := http.ListenAndServe(":3000", nil)
